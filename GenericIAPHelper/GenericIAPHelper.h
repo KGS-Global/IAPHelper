c582eed97db8bd3bf1173cdffa45d34a81e32cf6
Baker Mohammad Anas <baker.anas@bakers-mbp.lan>
2024-01-10 19:30:05 +0600

    Framework name changed to be unique in Pod

---
+++ GenericIAPHelper/GenericIAPHelper.h
@@ -0,0 +1,18 @@
+//
+//  GenericIAPHelper.h
+//  GenericIAPHelper
+//
+//  Created by Baker Mohammad Anas on 10/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GenericIAPHelper.
+FOUNDATION_EXPORT double GenericIAPHelperVersionNumber;
+
+//! Project version string for GenericIAPHelper.
+FOUNDATION_EXPORT const unsigned char GenericIAPHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericIAPHelper/PublicHeader.h>
+
+
